4550d68f10f2e0245035f1fc6d29840c676efb08
ASHVITHASHRI01 <114055661+ASHVITHASHRI01@users.noreply.github.com>
2023-02-24 10:23:43 +0530

    Add files via upload

---
+++ colour1/src/main/java/com/example/demo/Colour1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Colour1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Colour1Application.class, args);
+	}
+
+}
